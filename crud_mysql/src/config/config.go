7c0c25130c6fc888be2d2d95885fb15db6f054e5
abbas <abbasmomeny1994@gmail.com>
2024-04-07 17:32:28 +0330

    feat: connection to db

---
+++ crud_mysql/src/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"time"
+)
+
+func getDb() (db *sql.DB, err error) {
+
+	db, err = sql.Open("mysql", "root:root@/gopractice")
+	if err != nil {
+		log.Fatal("\nDb : connection failed \n ", err.Error())
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return
+}
